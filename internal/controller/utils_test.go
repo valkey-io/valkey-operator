9edac5a23f2624d6906cfdd67836c54cd5b0d5d5
Dan Molik <dan@hyperspike.io>
2025-10-31 08:02:47 -0400

    Add label and annotation utils
    Signed-off-by: Björn Svensson <bjorn.a.svensson@est.tech>
    Co-authored-by: Dan Molik <dan@hyperspike.io>

---
+++ internal/controller/utils_test.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2025 Valkey Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	valkeyv1 "valkey.io/valkey-operator/api/v1alpha1"
+)
+
+func TestLabels(t *testing.T) {
+	testLabels := map[string]string{
+		"app": "user-label",
+	}
+	cluster := &valkeyv1.ValkeyCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-resource",
+			Namespace: "default",
+			Labels:    testLabels,
+		},
+	}
+	result := labels(cluster)
+	if testLabels["app"] != result["app"] {
+		t.Errorf("Expected %v, got %v", testLabels["app"], result["app"])
+	}
+	if result["app.kubernetes.io/name"] != "valkey" {
+		t.Errorf("Expected %v, got %v", "valkey", result["app.kubernetes.io/name"])
+	}
+	if result["app.kubernetes.io/instance"] != "test-resource" {
+		t.Errorf("Expected %v, got %v", "test-resource", result["app.kubernetes.io/instance"])
+	}
+	result["app.kubernetes.io/component"] = "metrics"
+	if result["app.kubernetes.io/component"] != "metrics" {
+		t.Errorf("Expected %v, got %v", "metrics", result["app.kubernetes.io/component"])
+	}
+	result2 := labels(cluster)
+	if result2["app.kubernetes.io/component"] != "valkey-cluster" {
+		t.Errorf("Expected %v, got %v", "valkey-cluster", result2["app.kubernetes.io/component"])
+	}
+}
+
+func TestAnnotations(t *testing.T) {
+	testAnnotations := map[string]string{
+		"app": "user-annotation",
+	}
+	cluster := &valkeyv1.ValkeyCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "test-resource",
+			Namespace:   "default",
+			Annotations: testAnnotations,
+		},
+	}
+	result := annotations(cluster)
+	if testAnnotations["app"] != result["app"] {
+		t.Errorf("Expected %v, got %v", testAnnotations["app"], result["app"])
+	}
+}

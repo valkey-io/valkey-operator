7780156d14c07fac29165d06fc1f574ee1cf46a8
utdrmac <matthew.boehm@percona.com>
2026-01-20 13:24:22 -0600

    forgot file
    Signed-off-by: utdrmac <matthew.boehm@percona.com>

---
+++ internal/controller/config.go
@@ -0,0 +1,165 @@
+/*
+Copyright 2025 Valkey Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"crypto/sha256"
+	"embed"
+	"fmt"
+	"slices"
+
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	valkeyiov1alpha1 "valkey.io/valkey-operator/api/v1alpha1"
+)
+
+const (
+	configHashKey = "valkey.io/config-hash"
+)
+
+//go:embed scripts/*
+var scripts embed.FS
+
+// Create or update a default valkey.conf
+// If additional config is provided, append to the default map
+func (r *ValkeyClusterReconciler) upsertConfigMap(ctx context.Context, cluster *valkeyiov1alpha1.ValkeyCluster) error {
+
+	log := logf.FromContext(ctx)
+
+	// Embed readiness check script
+	readiness, err := scripts.ReadFile("scripts/readiness-check.sh")
+	if err != nil {
+		return err
+	}
+
+	// Embed liveness check script
+	liveness, err := scripts.ReadFile("scripts/liveness-check.sh")
+	if err != nil {
+		return err
+	}
+
+	serverConfig := `# Base operator config
+cluster-enabled yes
+protected-mode no
+cluster-node-timeout 2000
+`
+
+	// Shortcut
+	specConfig := cluster.Spec.ValkeySpec.Configuration
+
+	// If there are any user-defined config parameters
+	if len(specConfig) > 0 {
+
+		// Sort the config keys to keep consistent processing order
+		sortedConfig := make([]string, 0, len(specConfig))
+		for k := range specConfig {
+			sortedConfig = append(sortedConfig, k)
+		}
+		slices.Sort(sortedConfig)
+
+		// Build the config
+		serverConfig += "\n# Extra config\n"
+		for _, k := range sortedConfig {
+
+			if slices.Contains(valkeyiov1alpha1.NonUserOverrideConfigParameters, k) {
+				log.Error(nil, "Prohibited valkey server config", "parameter", k)
+				r.Recorder.Eventf(cluster, corev1.EventTypeWarning, "ConfigMapUpdateFailed", "Prohibited config: %v", k)
+				continue
+			}
+
+			serverConfig += k + " " + specConfig[k] + "\n"
+		}
+	}
+
+	// Look for, and fetch existing configMap for this cluster
+	serverConfigMapName := getConfigMapName(cluster.Name)
+	needCreateConfigMap := false
+
+	serverConfigMap := &corev1.ConfigMap{}
+	if err := r.Get(ctx, types.NamespacedName{
+		Name:      serverConfigMapName,
+		Namespace: cluster.Namespace,
+	}, serverConfigMap); err != nil {
+		if !apierrors.IsNotFound(err) {
+			log.Error(err, "failed to fetch server configmap")
+			return err
+		}
+
+		// ConfigMap not found. Create configMap object with contents
+		needCreateConfigMap = true
+		log.V(2).Info("creating server configMap", "name", serverConfigMapName)
+
+		serverConfigMap.ObjectMeta = metav1.ObjectMeta{
+			Name:      serverConfigMapName,
+			Namespace: cluster.Namespace,
+			Labels:    labels(cluster),
+		}
+		serverConfigMap.Data = map[string]string{
+			"readiness-check.sh": string(readiness),
+			"liveness-check.sh":  string(liveness),
+			"valkey.conf":        serverConfig,
+		}
+	}
+
+	// Register ownership of the configMap
+	if err := controllerutil.SetControllerReference(cluster, serverConfigMap, r.Scheme); err != nil {
+		log.Error(err, "Failed to grab ownership of server configMap")
+		r.Recorder.Eventf(cluster, corev1.EventTypeWarning, "ConfigMapCreationFailed", "Failed to grab ownership of server configMap: %v", err)
+		return err
+	}
+
+	// Calculate hash of new config parameters
+	newServerConfigHash := fmt.Sprintf("%x", sha256.Sum256([]byte(serverConfig)))
+
+	// Compare hash to the one already attached to the configMap (if previously exists)
+	if needsUpdate := upsertAnnotation(serverConfigMap, configHashKey, newServerConfigHash); !needsUpdate {
+		log.V(1).Info("Server configMap unchanged")
+		return nil
+	}
+
+	// Update the config with the newly changed config
+	serverConfigMap.Data["valkey.conf"] = serverConfig
+
+	// Need to create new configMap
+	if needCreateConfigMap {
+		if err := r.Create(ctx, serverConfigMap); err != nil {
+			log.Error(err, "Failed to create server configMap")
+			r.Recorder.Eventf(cluster, corev1.EventTypeWarning, "ConfigMapCreationFailed", "Failed to create server configMap: %v", err)
+			return err
+		} else {
+			r.Recorder.Eventf(cluster, corev1.EventTypeNormal, "ConfigMapCreated", "Created server configMap")
+			return nil
+		}
+	}
+
+	// Otherwise, update it
+	if err := r.Update(ctx, serverConfigMap); err != nil {
+		log.Error(err, "Failed to update server configMap")
+		r.Recorder.Eventf(cluster, corev1.EventTypeWarning, "ConfigMapUpdateFailed", "Failed to update server configMap: %v", err)
+		return err
+	}
+
+	r.Recorder.Eventf(cluster, corev1.EventTypeNormal, "ConfigMapUpdated", "Synchronized server configMap")
+
+	// All is good. Server configMap will be auto-mounted in the deployment
+	return nil
+}

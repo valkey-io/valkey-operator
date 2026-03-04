94febf3bc6523a0e941e2df3fca49866498da926
sandeep kunusoth <31273507+sandeepkunusoth@users.noreply.github.com>
2026-03-04 10:04:43 -0600

    chore(kubebuilder): Upgrade the Scaffold: v4.11.1 -> v4.13.0 (#97)
    fixes https://github.com/valkey-io/valkey-operator/issues/95
    
    ---------
    
    Signed-off-by: Sandeep Kunusoth <sandeepkunsoth000@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -32,7 +32,7 @@ import (
 )
 
 const (
-	certmanagerVersion = "v1.19.2"
+	certmanagerVersion = "v1.19.4"
 	certmanagerURLTmpl = "https://github.com/cert-manager/cert-manager/releases/download/%s/cert-manager.yaml"
 
 	defaultKindBinary  = "kind"
@@ -191,7 +191,7 @@ func UncommentCode(filename, target, prefix string) error {
 
 	idx := strings.Index(strContent, target)
 	if idx < 0 {
-		return fmt.Errorf("unable to find the code %q to be uncomment", target)
+		return fmt.Errorf("unable to find the code %q to be uncommented", target)
 	}
 
 	out := new(bytes.Buffer)

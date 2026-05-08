c7b51b361387e1f24b15b7724656bd00182ced97
sandeep kunusoth <31273507+sandeepkunusoth@users.noreply.github.com>
2026-05-08 15:03:44 -0500

    chore(kubebuilder): update scaffold v4.13.1 -> v4.14.0 (#161)
    fixes https://github.com/valkey-io/valkey-operator/issues/160
    
    - Add scaffold updates proposed by CI-step auto_update.
    - removed agents.md and e2e_test.go
    
    ---------
    
    Signed-off-by: Sandeep Kunusoth <sandeepkunsoth000@gmail.com>
    Signed-off-by: sandeep kunusoth <31273507+sandeepkunusoth@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -68,7 +68,7 @@ var _ = BeforeSuite(func() {
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: true,
+		ErrorIfCRDPathMissing: false,
 	}
 
 	// Retrieve the first found binary directory to allow running tests from IDEs

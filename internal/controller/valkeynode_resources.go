88b07c24931cc4b2d5c13782500e66685fe02f8e
sandeep kunusoth <31273507+sandeepkunusoth@users.noreply.github.com>
2026-06-07 16:32:17 -0500

    fix: Liveness and readiness probe scripts with sh for supporting alpine images (#205)
    This PR closes https://github.com/valkey-io/valkey-operator/issues/206
    
    ### Summary
    This PR addresses the crash loop experienced by Valkey clusters
    utilizing Alpine-based images(valkey/valkey:9.0.3-alpine) by eliminating
    bash dependencies from the probe scripts
    
    ### Features 
    
    Fixes compatibility with valkey alpine images
    
    ### Implementation
    
    Refactor readiness-check.sh and liveness-check.sh to work with alpine
    images.
    
    ### Testing
    
    tested with valkey alpine images and non alpine images. We could change
    one of the e2e test to alpine to cover this in e2e tests.
    
    ### Checklist
    
    Before submitting the PR make sure the following are checked:
    
    - [x] This Pull Request is related to one issue.
    - [x] Commit message explains what changed and why
    - [ ] Tests are added or updated.
    - [ ] Documentation files are updated.
    - [x] I have run pre-commit locally (`pre-commit run --all-files` or
    hooks on commit)
    
    ---------
    
    Signed-off-by: Sandeep Kunusoth <sandeepkunsoth000@gmail.com>

--- internal/controller/valkeynode_resources.go
+++ internal/controller/valkeynode_resources.go
@@ -202,7 +202,7 @@ func buildContainersDef(node *valkeyiov1alpha1.ValkeyNode) ([]corev1.Container, 
 				ProbeHandler: corev1.ProbeHandler{
 					Exec: &corev1.ExecAction{
 						Command: []string{
-							"/bin/bash",
+							"/bin/sh",
 							"-c",
 							"/scripts/liveness-check.sh",
 						},
@@ -218,7 +218,7 @@ func buildContainersDef(node *valkeyiov1alpha1.ValkeyNode) ([]corev1.Container, 
 				ProbeHandler: corev1.ProbeHandler{
 					Exec: &corev1.ExecAction{
 						Command: []string{
-							"/bin/bash",
+							"/bin/sh",
 							"-c",
 							"/scripts/liveness-check.sh",
 						},
@@ -234,7 +234,7 @@ func buildContainersDef(node *valkeyiov1alpha1.ValkeyNode) ([]corev1.Container, 
 				ProbeHandler: corev1.ProbeHandler{
 					Exec: &corev1.ExecAction{
 						Command: []string{
-							"/bin/bash",
+							"/bin/sh",
 							"-c",
 							"/scripts/readiness-check.sh",
 						},

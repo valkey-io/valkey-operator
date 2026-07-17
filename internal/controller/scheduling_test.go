878127b15ca1d354c6fec1b663e9f53f773cf996
Joseph Heyburn <jdheyburn@gmail.com>
2026-07-17 17:57:20 +0100

    fix: scheduling correct internal imports
    Signed-off-by: Joseph Heyburn <jdheyburn@gmail.com>

--- internal/controller/scheduling_test.go
+++ internal/controller/scheduling_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 
-	valkeyiov1alpha1 "valkey.io/valkey-operator/api/v1alpha1"
+	valkeyiov1alpha1 "github.com/valkey-io/valkey-operator/api/v1alpha1"
 )
 
 func TestEffectiveNodeSpread_Defaults(t *testing.T) {
